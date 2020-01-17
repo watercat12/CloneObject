266bba24181662e2ab3713f8ccb10c386ba28467
LocNguyen <locnguyen@teqnological.asia>
2020-01-17 14:52:29 +0700

    update

--- example.js
+++ example.js
@@ -3,4 +3,5 @@ var a = {
     name2: { name21: "", name22: "", name23: "" },
     name3: { name31: { name32: "", name33: "", name34: "" } }
 };
+ObjectHelper.deepCopy("");
 //# sourceMappingURL=example.js.map
--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,19 @@
-export declare function ObHelp():void
-export declare var ObjectHelper;
-export declare function copyObject();
+export as namespace ObjectHelper;
+
+/*~ This declaration specifies that the class constructor function
+ *~ is the exported object from the file
+ */
+export = ObjectHelper;
+
+/*~ Write your module's methods and properties in this class */
+declare class ObjectHelper<T> {
+
+  constructor(dataSource: T)
+  dataSource: any
+
+  public static setDataSource<T>(dataSource: T) : ObjectHelper<T>
+  public static deepCopy<T>(target: T): T
+  private setData(dataSource: any, key: string, value?: any) : ObjectHelper<T>
+  public copyWithParam(param: string, value?: any): ObjectHelper<T>
+  public getResult(): T
+}
