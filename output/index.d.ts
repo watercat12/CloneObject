459275ee1cfbb15c7685bd0649698ab57d19a597
LocNguyen <locnguyen@teqnological.asia>
2020-01-20 15:29:09 +0700

    v1.0.23

--- output/index.d.ts
+++ output/index.d.ts
@@ -1,27 +1,10 @@
 declare class ObjectHelper<T> {
-  constructor(dataSource: T);
-
-  private dataSource;
-
-  static setDataSource<T>(dataSource: T): ObjectHelper<T>;
-
-  static deepCopy<T>(target: T): T;
-
-  private setData;
-
-  copyWithParam(param: string, value?: any): ObjectHelper<T>;
-
-  getResult(): T;
+    constructor(dataSource: T);
+    private dataSource;
+    static setDataSource<T>(dataSource: T): ObjectHelper<T>;
+    static deepCopy<T>(target: T): T;
+    private setData;
+    copyWithParam(param: string, value?: any): ObjectHelper<T>;
+    getResult(): T;
 }
-
-declare global {
-  interface String {
-    padZero(): void;
-  }
-
-  interface Object {
-    print(): void;
-  }
-}
-
 export default ObjectHelper;
